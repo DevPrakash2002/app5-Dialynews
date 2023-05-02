cfa6c22154abe171185b3393491444225ab45a36
Dev Prakash <dev.tiwary5821@gmail.com>
2023-05-02 23:46:29 +0530

    2nd commit(Added language,links feature and updated link with a topic feature)

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
 api = "c2651126696a46999c8ac7c8ac8b7be8"
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-04-02&sortBy=publishedAt&apiKey=c2651126696a46999c8ac7c8ac8b7be8"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "sortBy=publishedAt&apiKey=c2651126696a46999c8ac7c8ac8b7be8&" \
+      "language=en"
 
 # Make Request
 request = requests.get(url)
@@ -12,9 +16,9 @@ request = requests.get(url)
 content = request.json()
 # Access the article title and description
 body = ''
-for article in content['articles']:
+for article in content['articles'][:20]:
     if article["title"] is not None:
-        body = body + article['title'] + '\n' + article["description"] + 2*'\n'
+        body = body + article['title'] + '\n' + article["description"] + '\n' + article["url"] + 2*'\n'
 
 mess = f"""\
 Subject: Hi Here's Your Daily News
